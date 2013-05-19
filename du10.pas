bdb5235c1630cc11e3e74964d923a7d3dcda9b7b
kirecek <er.jankovic@gmail.com>
2013-05-19 16:08:34 +0200

    Update du10.pas

--- du10.pas
+++ du10.pas
@@ -1,143 +1,125 @@
-program knightstour;
-     
-{$mode objfpc}{$H+}
-     
-uses
-  cthreads, sysutils, classes;
-     
-const 
-	N = 4; 
-	M = 4;
-	T = 3; 
-  
-type 
-	TKocka = record
-		X,Y: integer;
-	end; 
-	
-	TTriomin = array [0..2] of TKocka;
-     
-var
-	plocha: array [0..N-1,0..M-1] of integer;
-   x1: array [0..T] of integer = (0,0,1,0); 
-   x2: array [0..T] of integer = (1,0,1,1);
-   x3: array [0..T] of integer = (1,1,0,0);
-   y1: array [0..T] of integer = (0,0,0,0);
-   y2: array [0..T] of integer = (0,1,1,0);
-	y3: array [0..T] of integer = (1,1,1,1);              
+program Triomin;
 
-function skontroluj: boolean;
+{$mode objfpc}{$H+}
+
+uses
+	cthreads, sysutils, classes;
+
+const 
+	N = 3;
+	M = 3;	
+
+type
+	TMove = array [1..3] of Integer;
+
+var
+	plocha: array [1..N,1..M] of Integer;
+	x1: array[1..12] of Integer = (-1,-1,0,0,0,0,1,1,0,0,0,0);
+	x2: array[1..12] of Integer = (-1,-1,0,0,0,0,1,1,1,1,-1,-1);
+	x3: array[1..12] of Integer = (0,0,-1,1,-1,1,0,0,1,1,-1,-1);
+	y1: array[1..12] of Integer = (0,0,0,0,0,0,0,0,0,0,0,0);
+	y2: array[1..12] of Integer = (-1,1,-1,-1,1,1,-1,1,0,0,0,0);
+	y3: array[1..12] of Integer = (-1,1,-1,-1,1,1,-1,1,-1,1,-1,1);
+
+procedure VytvorPlochu;
 var
 	x,y: integer;
 begin
-	result:= true;
-	for x:= 0 to N-1 do
-		for y:= 0 to M-1 do
-			if plocha[x,y] = 0 then
-			begin
-				result:= false;
-				exit;
-			end;
-end;
-  
-procedure vytvor_plochu;
-var
-	x,y: integer;
-begin
-	for x:= 0 to N-1 do
-		for y:= 0 to M-1 do
-		begin
+	for x:= 1 to N do 
+	begin
+		for y:= 1 to M do 
 			plocha[x,y]:= 0;
-		end;
-end;  
- 
-procedure vypis_plochu;
+	end;
+end; // koniec funkcie
+
+procedure VypisPlochu;
 var
 	x,y: integer;
 begin
-	for x:= 0 to N-1 do begin
-		for y:= 0 to M-1 do
+	for x:= 1 to N do 
+	begin
+		for y:= 1 to M do 
 			write(Format(' %2d ', [plocha[x,y]]));
-		writeln;
+	writeln;
 	end;
-end; 
-                   
-function je_volne(triomin: ttriomin): boolean;
+end;
+
+function Skontroluj: Boolean;
 var
-	k: integer;
+	x, y: Integer;
 begin
-	for k:= 0 to 2 do begin
-		if ((triomin[k].x >= 0) and (triomin[k].x < N) and (triomin[k].y >= 0) 
-		and (triomin[k].y < N) and (plocha[triomin[k].x,triomin[k].y] = 0)) then 
+	for x:= 1 to N do
+		for y:= 1 to M do 
+			if plocha[x,y] = 0 then 
+			begin
+				Result:= False;
+				Exit;
+			end;
+	Result:= True;
+end;
+
+function VolnyStvorec(X, Y: Integer): Boolean;
+begin	
+	if ( (X > 0) and (X <= N) and (Y>0) and (Y<=M) and (plocha[x,y] = 0)) then
+		Result:= True
+	else
+		Result:= False;	
+end;
+
+function UlozTriominy(X, Y, TrimID: Integer): Boolean;
+var
+	k,j: Integer;
+	dalsiX, dalsiY : TMove;
+begin
+	if Skontroluj then begin
+		Result:= True;
+		Exit;
+	end;
+	
+	for k:= 1 to 12 do 
+	begin
+		dalsiX[1]:= X + x1[k];
+		dalsiX[2]:= X + x2[k];
+		dalsiX[3]:= X + x3[k];
+		dalsiY[1]:= Y + y1[k];
+		dalsiY[2]:= Y + y2[k];
+		dalsiY[3]:= Y + y3[k];
+		
+		if(VolnyStvorec(dalsiX[1],dalsiY[1]) and VolnyStvorec(dalsiX[2],dalsiY[2]) and VolnyStvorec(dalsiX[3],dalsiY[3])) then
 		begin
-			result:= true;
-		end
-		else begin
-			result:= false;
-			exit;
+			plocha[dalsiX[1],dalsiY[1]]:= TrimID;
+			plocha[dalsiX[2],dalsiY[2]]:= TrimID;
+			plocha[dalsiX[3],dalsiY[3]]:= TrimID;
+			
+			//if(UlozTriominy(dalsiX[1],dalsiY[1],TrimID+1) or UlozTriominy(dalsiX[2],dalsiY[2],TrimID+1) or UlozTriominy(dalsiX[3],dalsiY[3],TrimID+1)) then
+			if(UlozTriominy(X+1,Y+1,TrimID+1) or UlozTriominy(X+1,Y-1,TrimID+1) or UlozTriominy(X-1,Y+1,TrimID+1) or UlozTriominy(X-1,Y-1,TrimID+1)) then
+			begin
+				Result:= True;
+				Exit;
+			end
+			else 
+			begin
+				plocha[dalsiX[1],dalsiY[1]]:= 0;
+				plocha[dalsiX[2],dalsiY[2]]:= 0;
+				plocha[dalsiX[3],dalsiY[3]]:= 0;
+			end;
 		end;	
 	end;
+	Result:= False;
 end;
-        
-           
-function vyries(x, y, p: integer): boolean;
-var
-	k, next_x, next_y: integer;
-	triomin: ttriomin;
+
 begin
-	result:= false;
-           
-	if skontroluj then begin
-		result:= true;
-		exit;  
-	end;
-           
-	for k:= 0 to T do
-	begin
-		triomin[0].x:= x + x1[k];
-		triomin[0].y:= y + y1[k];
-		triomin[1].x:= x + x2[k];
-  		triomin[1].y:= y + y2[k];
-		triomin[2].x:= x + x3[k];
-		triomin[2].y:= y + y3[k];
-                   
-		if je_volne(triomin) then
-		begin      
-			plocha[triomin[0].x,triomin[0].y]:= p;
-			plocha[triomin[1].x,triomin[1].y]:= p;
-			plocha[triomin[2].x,triomin[2].y]:= p;
-				
-			if(vyries(x+1,y,p+1) or vyries(x-1,y,p+1) or 
-			vyries(x,y+1,p+1) or vyries(x,y-1,p+1)) then
-			begin
-				result:= true;
-				exit;
-			end
-			else begin
-				plocha[triomin[0].x,triomin[0].y]:= 0;
-				plocha[triomin[1].x,triomin[1].y]:= 0;
-				plocha[triomin[2].x,triomin[2].y]:= 0;
-      	end;
-		end;
-	end;
-	result:= false;
-end;   
-           
-procedure trim_bt;
-begin
-     
-	if not vyries(0,0,1) then
-	begin
-		writeln('Neexistuje riesenie!');
-		exit;
+	VytvorPlochu;
+	Plocha[1,1]:= -1; // -1 = prekazky
+	Plocha[1,3]:= -1;
+	Plocha[3,1]:= -1;
+	writeln('Pred vyplnenim: ');
+	VypisPlochu;
+	writeln;
+	if UlozTriominy(1,1,1) then begin
+		writeln('Po vyplneni: ');
+		VypisPlochu;
 	end
 	else
-		vypis_plochu;
-end;           
-    //*****************************************************        
-begin
-	vytvor_plochu;
-	//vypis_plochu;
-	trim_bt;
-	writeln;
+		writeln('Triominy sa nedaju poukladat!!');
 end.
